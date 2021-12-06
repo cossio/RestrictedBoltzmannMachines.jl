10bb83de749ee5135a2753b3beb1fc97d6843adc
cossio <j.cossio.diaz@gmail.com>
2021-12-06 13:16:57 +0100

    add MNIST example to docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,19 @@
-using Documenter, RestrictedBoltzmannMachines
+using Documenter, Literate
 import RestrictedBoltzmannMachines as RBMs
+
+examples = Vector{Pair{String,String}}()
+for ex in readdir("literate"; join=true)
+    name = first(splitext(basename(ex)))
+    Literate.markdown(ex, "literate/"; name=name)
+    push!(examples, name => "literate/$name.md")
+end
 
 makedocs(
     modules=[RestrictedBoltzmannMachines],
-    sitename="RestrictedBoltzmannMachines.jl"
+    sitename="RestrictedBoltzmannMachines.jl",
+    pages = [
+        "Examples" => examples
+    ]
 )
 
 deploydocs(
---
+++ docs/src/literate/mnist.jl
@@ -0,0 +1,35 @@
+using CairoMakie
+import MLDatasets
+import RestrictedBoltzmannMachines as RBMs
+
+#=
+First we load the MNIST dataset.
+=#
+train_x, train_y = MLDatasets.MNIST.traindata();
+tests_x, tests_y = MLDatasets.MNIST.testdata();
+
+#=
+We will train an RBM with binary (0,1) visible and hidden units.
+Therefore we binarize the data first.
+=#
+train_x = float(train_x .≥ 0.5);
+tests_x = float(tests_x .≥ 0.5);
+train_y = float(train_y);
+tests_y = float(tests_y);
+
+#=
+Initialize an RBM with 100 hidden units.
+=#
+rbm = RBMs.RBM(RBMs.Binary(28,28), RBMs.Binary(100), randn(28, 28, 100) / 28);
+
+#=
+Train the RBM on the data.
+This returns a MVHistory object (from https://github.com/JuliaML/ValueHistories.jl),
+containing things like the pseudo-likelihood of the data during training.
+=#
+history = RBMs.train!(rbm, train_x; epochs=10, batchsize=128);
+
+#=
+Plot log-pseudolikelihood during learning.
+=#
+lines(get(history, :lpl))
