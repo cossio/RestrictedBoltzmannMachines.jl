c6c882ed8efe59cc008b8c352d2a49d42a9d33d7
cossio <j.cossio.diaz@gmail.com>
2021-12-06 13:24:00 +0100

    fix pgm.jl tests

--- test/compare_to_pgm/pgm.jl
+++ test/compare_to_pgm/pgm.jl
@@ -11,10 +11,10 @@ values of energies, etc. with my code. =#
     θ = vec(readdlm("compare_to_pgm/RBM_bin_bin_gh.txt"));
     w = readdlm("compare_to_pgm/RBM_bin_bin_w.txt")';
 
-    v = readdlm("compare_to_pgm/RBM_bin_v.txt")
-    h = readdlm("compare_to_pgm/RBM_bin_h.txt")
-    E = readdlm("compare_to_pgm/RBM_bin_bin_E.txt")
-    F = readdlm("compare_to_pgm/RBM_bin_bin_F.txt")
+    v = readdlm("compare_to_pgm/RBM_bin_v.txt")'
+    h = readdlm("compare_to_pgm/RBM_bin_h.txt")'
+    E = vec(readdlm("compare_to_pgm/RBM_bin_bin_E.txt"))
+    F = vec(readdlm("compare_to_pgm/RBM_bin_bin_F.txt"))
 
     rbm = RBMs.RBM(RBMs.Binary(g), RBMs.Binary(θ), w)
     @test RBMs.energy(rbm, v, h) ≈ E
