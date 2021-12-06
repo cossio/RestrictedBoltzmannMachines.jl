e173e44ad1a1b757683c64284d71cfc4822b3c18
cossio <j.cossio.diaz@gmail.com>
2021-12-06 15:03:27 +0100

    move literate source files outside docs/src

--- docs/make.jl
+++ docs/make.jl
@@ -2,13 +2,16 @@ using Documenter, Literate
 import RestrictedBoltzmannMachines as RBMs
 
 examples = Vector{Pair{String,String}}()
-for ex in readdir("src/literate"; join=true)
+for ex in readdir("literate"; join=true)
     if endswith(ex, ".jl")
         name = first(splitext(basename(ex)))
+        @show name
         Literate.markdown(ex, "src/literate"; name=name)
         push!(examples, name => "literate/$name.md")
     end
 end
+
+@show examples
 
 makedocs(
     modules=[RBMs],
