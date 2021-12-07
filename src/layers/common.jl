a8dd511cd5adf121cb93b71cb34c28cb45ceb4f8
cossio <j.cossio.diaz@gmail.com>
2021-12-07 13:32:17 +0100

    remove conversions between layers
    There are several ways to do these conversions.
    It should be done explicitly by the user.

--- src/layers/common.jl
+++ src/layers/common.jl
@@ -1,38 +1,15 @@
-"""
-    batchmul(fields, x)
-
-Forms the product fields * x, summing over the fields dimension while
-leaving the batch dimensions.
-"""
-function batchmul(fields::AbstractArray, x::AbstractArray)
-    @assert size(x) == (size(fields)..., size(x)[end])
-    return reshape(x, length(fields), size(x)[end])' * vec(fields)
-end
-
-function energy(layer::Union{Binary,Spin,Potts}, x::AbstractArray)
+function energy(layer::Union{Binary, Spin, Potts}, x::AbstractArray)
     @assert size(x) == (size(layer)..., size(x)[end])
-    return -batchmul(layer.θ, x)
+    return -reshape(x, length(layer), size(x)[end])' * vec(layer.θ)
 end
 
-const _LayersWithTheta = Union{Binary,Spin,Potts,Gaussian,StdGaussian,ReLU,pReLU}
-Base.ndims(layer::_LayersWithTheta) = ndims(layer.θ)
-Base.size(layer::_LayersWithTheta) = size(layer.θ)
-Base.size(layer::_LayersWithTheta, d::Int) = size(layer.θ, d)
-Base.length(layer::_LayersWithTheta) = length(layer.θ)
+const _ThetaLayers = Union{Binary, Spin, Potts, Gaussian, StdGaussian, ReLU, pReLU}
+Base.ndims(layer::_ThetaLayers) = ndims(layer.θ)
+Base.size(layer::_ThetaLayers) = size(layer.θ)
+Base.size(layer::_ThetaLayers, d::Int) = size(layer.θ, d)
+Base.length(layer::_ThetaLayers) = length(layer.θ)
 
 layerdims(layer) = ntuple(identity, ndims(layer))
-
-Binary(layer::Union{Spin,Potts}) = Binary(layer.θ)
-Spin(layer::Union{Binary,Potts}) = Spin(layer.θ)
-Potts(layer::Union{Binary,Spin}) = Potts(layer.θ)
-
-ReLU(layer::Gaussian) = ReLU(layer.θ, layer.γ)
-Gaussian(layer::ReLU) = Gaussian(layer.θ, layer.γ)
-
-function dReLU(p::Union{Gaussian, ReLU}, n::Union{Gaussian, ReLU})
-    @assert size(p) == size(n)
-    return dReLU(p.θ, -n.θ, p.γ, n.γ)
-end
 
 function pReLU(l::dReLU)
     θ = @. (l.θp * l.γn + l.θn * l.γp) / (l.γp + l.γn)
