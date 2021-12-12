09ac7251467a81655223a217f4dad47f78a28a33
cossio <j.cossio.diaz@gmail.com>
2021-12-12 11:59:18 +0100

    reorder tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,14 +3,15 @@ using SafeTestsets, Random, Test
 @time @safetestset "util" begin include("util.jl") end
 @time @safetestset "onehot" begin include("onehot.jl") end
 @time @safetestset "minibatches" begin include("minibatches.jl") end
-@time @safetestset "truncnorm" begin include("truncnorm/truncnorm.jl") end
-@time @safetestset "truncnorm rejection" begin include("truncnorm/rejection.jl") end
-@time @safetestset "layers" begin include("layers.jl") end
-@time @safetestset "rbm" begin include("rbm.jl") end
-@time @safetestset "pgm" begin include("compare_to_pgm/pgm.jl") end
-@time @safetestset "pseudolikelihood" begin include("pseudolikelihood.jl") end
 @time @safetestset "zerosum" begin include("zerosum.jl") end
 @time @safetestset "initialization" begin include("initialization.jl") end
+@time @safetestset "layers" begin include("layers.jl") end
+@time @safetestset "rbm" begin include("rbm.jl") end
+@time @safetestset "pseudolikelihood" begin include("pseudolikelihood.jl") end
+@time @safetestset "pgm" begin include("compare_to_pgm/pgm.jl") end
+
+@time @safetestset "truncnorm" begin include("truncnorm/truncnorm.jl") end
+@time @safetestset "truncnorm rejection" begin include("truncnorm/rejection.jl") end
 
 #@time @safetestset "regularize" begin include("regularize.jl") end
 # @time @safetestset "learning rate decay schedules" begin include("lr_schedules.jl") end
