9a79d3a220ad1884c01fbb07df0d495fff072e87
cossio <j.cossio.diaz@gmail.com>
2021-12-16 03:28:56 +0100

    MNIST update paper ref

--- docs/src/literate/MNIST.jl
+++ docs/src/literate/MNIST.jl
@@ -274,7 +274,7 @@ fig
 # ## Whitening the data
 
 #=
-<https://jmlr.org/papers/volume17/14-237/14-237.pdf>
-<http://www.cs.toronto.edu/~tang/papers/RbmZM.pdf>
+<https://www.jmlr.org/beta/papers/v17/14-237.html>,
+<http://www.cs.toronto.edu/~tang/papers/RbmZM.pdf>,
 <https://doi.org/10.1007/978-3-642-35289-8_3>
 =#
