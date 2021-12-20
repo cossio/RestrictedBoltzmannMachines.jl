ae22c09e9f79cc2909de537a72ac6abb27451f90
Documenter.jl <documenter@juliadocs.github.io>
2021-12-20 19:10:44 +0000

    build based on 5c6c58c

--- dev/literate/drelu.jl
+++ dev/literate/drelu.jl
@@ -31,7 +31,7 @@ nothing #hide
 Now we sample our layer to collect some data.
 =#
 
-data = RBMs.sample_from_inputs(layer, zeros(size(layer)..., 10^6))
+data = RBMs.sample(layer, zeros(size(layer)..., 10^6))
 nothing #hide
 
 #=
