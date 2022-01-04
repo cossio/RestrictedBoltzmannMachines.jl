8cfce01838ce259f2462fdd040ff16d06d10743a
cossio <j.cossio.diaz@gmail.com>
2022-01-04 15:42:40 +0100

    ais wip

---
+++ src/ais.jl
@@ -0,0 +1,3 @@
+# Normal AIS tends to ovestimate likelihood.
+# Use http://proceedings.mlr.press/v38/burda15.html to lower-bound the log-likelihood
+# together we have an interval of where log-likelihood lies.
