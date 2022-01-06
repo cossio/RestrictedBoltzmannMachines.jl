8cae60f94f957e96e465977d697f6bdd1a12c5e9
cossio <j.cossio.diaz@gmail.com>
2022-01-06 20:34:53 +0100

    make sure MKL example uses OpenBLAS first

--- docs/src/literate/mkl.jl
+++ docs/src/literate/mkl.jl
@@ -6,6 +6,12 @@ in this package, it is advisable to use MKL.
 Let's do a quick comparison.
 =#
 
+# To be sure we are using OpenBLAS first, we can run:
+
+using LinearAlgebra
+LinearAlgebra.__init__() # don't need this on a fresh Julia session
+BLAS.get_config()
+
 # We use Float32 for the test.
 
 Float = Float32
@@ -26,7 +32,7 @@ RBMs.initialize!(rbm, train_x)
 RBMs.cd!(rbm, train_x; epochs=10, batchsize=128, verbose=true, steps=1)
 nothing #hide
 
-# Since we haven't loaded MKL, this first run used OpenBLAS.
+# Since we haven't loaded MKL, this first run should have used OpenBLAS.
 # You can confirm that by doing:
 
 using LinearAlgebra
@@ -39,23 +45,23 @@ If you don't have it installed, run first `pkg> add MKL`.
 =#
 
 using MKL
-
-#=
-In Julia 1.7 (which I assume you have), that's all you need to do.
-See <https://julialang.org/blog/2021/11/julia-1.7-highlights/#libblastrampoline_mkljl>.
-Now LinearAlgebra routines should forward to MKL:
-=#
-
+MKL.__init__() # don't need this on a fresh Julia session
 BLAS.get_config()
 
 #=
-which should tell you that you are using libmkl.
+In Julia 1.7 (which I assume you have), that's all you need to do.
+If you are on a fresh Julia session where `MKL` was not loaded before, you can skip
+the line `MKL.__init__()`.
+See <https://julialang.org/blog/2021/11/julia-1.7-highlights/#libblastrampoline_mkljl>.
+Now LinearAlgebra routines should forward to MKL (which should be confimed by the output
+of `BLAS.get_config()` above).
+
 If for some reason you happen to want to go back to OpenBlAS, you can do
 `LinearAlgebra.__init()__`.
 And then `MKL.__init__()` to go back to MKL again, and so on.
-=#
 
-# Now let's rerun the RBM training.
+Now let's rerun the RBM training.
+=#
 
 rbm = RBMs.RBM(RBMs.Binary(Float,28,28), RBMs.Binary(Float,128), zeros(Float,28,28,128))
 RBMs.initialize!(rbm, train_x)
