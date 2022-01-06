d6d6dd4bc75516c81553aadbbeda3202813eb76e
cossio <j.cossio.diaz@gmail.com>
2022-01-06 01:25:39 +0100

    do tests with MKL

--- test/tests_init.jl
+++ test/tests_init.jl
@@ -1,3 +1,7 @@
 using Test, Random, LinearAlgebra, Statistics, DelimitedFiles
 import Zygote, Flux, Distributions, SpecialFunctions, LogExpFunctions, QuadGK, NPZ
 import RestrictedBoltzmannMachines as RBMs
+
+#= As far as I know, Github Actions uses Intel CPUs.
+So it is faster to use MKL than OpenBLAS. =#
+using MKL
