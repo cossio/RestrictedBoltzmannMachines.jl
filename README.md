9f6a6f319201654d4f8c1048a5eed79783165f5a
cossio <j.cossio.diaz@gmail.com>
2022-01-06 12:40:46 +0100

    weight_normalization.jl and Readme
    readme
    
    fix docstring

--- README.md
+++ README.md
@@ -7,24 +7,54 @@
 [![codecov](https://codecov.io/gh/cossio/RestrictedBoltzmannMachines.jl/branch/master/graph/badge.svg?token=O5P8LQTVF3)](https://codecov.io/gh/cossio/RestrictedBoltzmannMachines.jl)
 ![GitHub repo size](https://img.shields.io/github/repo-size/cossio/RestrictedBoltzmannMachines.jl)
 ![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/cossio/RestrictedBoltzmannMachines.jl)
+[![Code Style: Blue](https://img.shields.io/badge/code%20style-blue-4495d1.svg)](https://github.com/invenia/BlueStyle)
 
-Restricted Boltzmann Machines in Julia.
-Related packages:
+Train and sample [Restricted Boltzmann Machines](https://en.wikipedia.org/wiki/Restricted_Boltzmann_machine) in Julia.
+See the [Documentation](https://cossio.github.io/RestrictedBoltzmannMachines.jl/stable) for details.
 
-https://github.com/dfdx/Boltzmann.jl
-
-https://github.com/stefan-m-lenz/BoltzmannMachines.jl
-
-In contrast to those packages, here we adopt a generic approach that allows defining any kind of unit potentials (such as ReLU units, which are not included in those two packages), and use them as visible or hidden layers of the RBM.
-On the other hand, those two repos support deep Boltzmann machines, which we do not.
-
-For a Python 3 implementation checkout https://github.com/jertubiana/PGM, which originates the `dReLU` layer and showcases nice applications to protein and neural data.
+## Installation
 
 This package is registered.
 Install with:
 
-```
-] add RestrictedBoltzmannMachines
+```julia
+using Pkg
+Pkg.add("RestrictedBoltzmannMachines")
 ```
 
-For examples see the documentation (check out the badges above).
+Alternatively,
+
+```julia
+pkg> add RestrictedBoltzmannMachines
+```
+
+from the Pkg REPL.
+
+This package does not export any symbols.
+Since the name `RestrictedBoltzmannMachines` is verbose, I recommend to import it as:
+
+```julia
+import RestrictedBoltzmannMachines as RBMs
+```
+
+## Related packages
+
+Other packages implementing Restricted Boltzmann Machines in Julia:
+
+- https://github.com/dfdx/Boltzmann.jl
+- https://github.com/stefan-m-lenz/BoltzmannMachines.jl
+
+Here are some differences:
+
+- **Genericity:** In contrast to those packages, here we adopt a generic approach that allows defining any kind of unit potentials (such as ReLU or dReLU units, which are not included in those two packages), and use them as visible or hidden layers of the RBM.
+- **Easily extensible:** This also means the package is easily extensible to other layer types, or to modified training procedures. See `src/train/` directory for exploration of training schemes (CD, PCD, centering, etc.). Other packages tend to support only CD or PCD.
+- **Shallow**: On the other hand, those two repos support deep Boltzmann machines, which we do not.
+
+For a Python 3 implementation see https://github.com/jertubiana/PGM, which originates the `dReLU` layer and showcases applications to protein and neural data.
+
+## ToDo
+
+Some features that should be implemented in the future:
+
+- AIS estimation of the partition function.
+- Unbiased contrastive divergence sampling algorithm.
--- src/train/weight_normalization.jl
+++ src/train/weight_normalization.jl
@@ -1,67 +1,50 @@
-"""
-    pcd_norm!(rbm, data)
+struct WeightNorm{Tg<:AbstractArray, Tv<:AbstractArray}
+    g::Tg
+    v::Tv
+    """
+        WeightNorm(g, v)
 
-Trains the RBM on data, using the weight normalization heuristic.
-See Salimans & Kingma 2016,
-<https://proceedings.neurips.cc/paper/2016/hash/ed265bc903a5a097f61d3ec064d96d2e-Abstract.html>.
-"""
-function pcd_norm!(rbm::RBM, data::AbstractArray;
-    batchsize = 1,
-    epochs = 1,
-    optimizer = ADAM(), # optimizer algorithm
-    history::MVHistory = MVHistory(), # stores training log
-    verbose::Bool = true,
-    wts::Wts = nothing, # data point weights
-    steps::Int = 1, # Monte Carlo steps to update fantasy particles
-)
-    @assert size(data) == (size(rbm.visible)..., _nobs(data))
-    @assert isnothing(wts) || _nobs(data) == _nobs(wts)
-
-    # v, g notation from Salimans & Kingma 2016
-    wg = sqrt.(sum(abs2, rbm.w; dims=1:ndims(rbm.visible))) # weight norms
-    wv = rbm.w ./ wg # normalized directions
-    vnorm = sqrt.(sum(abs2, wv; dims=1:ndims(rbm.visible)))
-
-    # initialize fantasy chains
-    _idx = rand(1:_nobs(data), batchsize)
-    _vm = selectdim(data, ndims(data), _idx)
-    vm = sample_v_from_v(rbm, _vm; steps = steps)
-
-    for epoch in 1:epochs
-        batches = minibatches(data, wts; batchsize = batchsize)
-        Δt = @elapsed for (b, (vd, wd)) in enumerate(batches)
-            # update fantasy chains
-            vm = sample_v_from_v(rbm, vm; steps = steps)
-            # estimate gradients
-            ∂ = ∂contrastive_divergence(rbm, vd, vm; wd = wd, wm = wd)
-            # update visible and hidden layers as usual
-            update!(optimizer, rbm.visible, ∂.visible)
-            update!(optimizer, rbm.hidden, ∂.hidden)
-
-            #= Now update the weights following Salimans & Kingma 2016.
-            Only the update of the weights is affected by the normalization scheme. =#
-            ∂gv = ∂wnorm(∂.w, rbm.w, wg, wv)
-
-            update!(optimizer, wg, ∂gv.g)
-            update!(optimizer, wv, ∂gv.v)
-
-            # Update the RBM weights using the updated values of v, g
-            vnorm .= sqrt.(sum(abs2, wv; dims=1:ndims(rbm.visible)))
-            rbm.w .= wg .* wv ./ vnorm
-
-            push!(history, :epoch, epoch)
-            push!(history, :batch, b)
-        end
-
-        lpl = batch_mean(log_pseudolikelihood(rbm, data), wts)
-        push!(history, :lpl, lpl)
-        if verbose
-            Δt_ = round(Δt, digits=2)
-            lpl_ = round(lpl, digits=2)
-            println("epoch $epoch/$epochs ($(Δt_)s), log(pseudolikelihood)=$lpl_")
-        end
+    Implements the the weight normalization heuristic for training neural networks.
+    See Salimans & Kingma 2016,
+    <https://proceedings.neurips.cc/paper/2016/hash/ed265bc903a5a097f61d3ec064d96d2e-Abstract.html>.
+    """
+    function WeightNorm(g::AbstractArray, v::AbstractArray)
+        @assert size(g) .== size(v) .|| size(g) .== 1
+        return new{typeof(g), typeof(v)}(v, g)
     end
-    return history
+end
+
+@doc raw"""
+        WeightNorm(rbm, λ)
+
+Returns a re-parameterization of `rbm` weights, defined by:
+
+```math
+\mathbf{w}_\mu = g_\mu \frac{\mathbf{v}_\mu}{\|\mathbf{v}_\mu\|}
+```
+
+where the parameter ``g_\mu`` encodes the norm of the weight pattern attached to
+hidden unit ``\mu``, while ``\mathbf{v}_\mu`` encodes its direction.
+
+The constructor `WeightNorm(rbm, λ)` initializes ``g_\mu`` and ``\mathbf{v}_\mu``
+assuming that the vectors ``\mathbf{v}_\mu`` have norms `λ`.
+More precisely, we assume that:
+
+```julia
+λ = sqrt.(sum(abs2, v; dims=1:ndims(rbm.visible)))
+```
+"""
+function WeightNorm(rbm::RBM, λ::AbstractArray)
+    @assert size(λ) == (ntuple(Returns(1), ndims(rbm.visible))..., size(rbm.hidden)...)
+    g = sqrt.(sum(abs2, rbm.w; dims=1:ndims(rbm.visible)))
+    v = λ .* rbm.w ./ g
+    return new{typeof(g), typeof(v)}(v, g)
+end
+
+function update_w_from_vg!(rbm::RBM, wn::WeightNorm)
+    λ = sqrt.(sum(abs2, wn.v; dims=1:ndims(rbm.visible)))
+    rbm.w .= wn.g .* wn.v ./ λ
+    return rbm
 end
 
 @doc raw"""
@@ -83,4 +66,74 @@ function ∂wnorm(∂w::AbstractArray, w::AbstractArray, g::AbstractArray, v::Ab
     ∂g = sum!(similar(g), ∂w .* w ./ g)
     ∂v = (∂w - ∂g .* w ./ g) .* w ./ v
     return (g = ∂g, v = ∂v)
+end
+
+function ∂wnorm(∂w::AbstractArray, rbm::RBM, wn::WeightNorm)
+    @assert size(∂w) == size(rbm.w) == size(wn.v)
+    return ∂wnorm(∂w, rbm.w, wn.g, wn.v)
+end
+
+function ∂contrastive_divergence(
+    rbm::RBM, wn::WeightNorm, args...; kwargs...
+)
+    ∂ = ∂contrastive_divergence(rbm, args...; kwargs...)
+    ∂norm = ∂wnorm(∂.w, rbm, wn)
+    return (visible = ∂.visible, hidden = ∂.hidden, g = ∂norm.g, v = ∂norm.v)
+end
+
+function update!(optimizer, rbm::RBM, wn::WeightNorm, ∂::NamedTuple)
+    update!(optimizer, rbm.visible, ∂.visible)
+    update!(optimizer, rbm.hidden, ∂.hidden)
+    update!(optimizer, wn.g, ∂.g)
+    update!(optimizer, wn.v, ∂.v)
+    update_w_from_vg!(rbm, wn)
+end
+
+"""
+    pcd!(rbm, weight_norm::WeightNorm, data)
+
+Trains the RBM on data, using the weight normalization heuristic.
+See Salimans & Kingma 2016,
+<https://proceedings.neurips.cc/paper/2016/hash/ed265bc903a5a097f61d3ec064d96d2e-Abstract.html>.
+"""
+function pcd!(rbm::RBM, wn::WeightNorm, data::AbstractArray;
+    batchsize = 1,
+    epochs = 1,
+    optimizer = default_optimizer(_nobs(data), batchsize, epochs), # optimizer algorithm
+    history::MVHistory = MVHistory(), # stores training log
+    verbose::Bool = true,
+    wts::Wts = nothing, # data point weights
+    steps::Int = 1, # Monte Carlo steps to update fantasy particles
+)
+    @assert size(data) == (size(rbm.visible)..., size(data)[end])
+    @assert isnothing(wts) || _nobs(data) == _nobs(wts)
+
+    # initialize fantasy chains
+    _idx = rand(1:_nobs(data), batchsize)
+    _vm = selectdim(data, ndims(data), _idx)
+    vm = sample_v_from_v(rbm, _vm; steps = steps)
+
+    for epoch in 1:epochs
+        batches = minibatches(data, wts; batchsize = batchsize)
+        Δt = @elapsed for (b, (vd, wd)) in enumerate(batches)
+            # update fantasy chains
+            vm = sample_v_from_v(rbm, vm; steps = steps)
+            # compute contrastive divergence gradient
+            ∂ = ∂contrastive_divergence(rbm, wn, vd, vm; wd = wd)
+            # update parameters using gradient
+            update!(optimizer, rbm, wn, ∂)
+
+            push!(history, :epoch, epoch)
+            push!(history, :batch, b)
+        end
+
+        lpl = batch_mean(log_pseudolikelihood(wnorm_rbm, data), wts)
+        push!(history, :lpl, lpl)
+        if verbose
+            Δt_ = round(Δt, digits=2)
+            lpl_ = round(lpl, digits=2)
+            println("epoch $epoch/$epochs ($(Δt_)s), log(pseudolikelihood)=$lpl_")
+        end
+    end
+    return history
 end
