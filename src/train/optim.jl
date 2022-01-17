790c6f729f7d4a6cde132a5ac7db39b966b93f18
cossio <j.cossio.diaz@gmail.com>
2022-01-17 19:02:12 +0100

    remove comments

--- src/train/optim.jl
+++ src/train/optim.jl
@@ -1,26 +1,4 @@
-"""
-    ExpDecay(η = 0.001, decay = 0.1, decay_step = 1000, clip = 1e-4, start = 1)
-
-Discount the learning rate `η` by the factor `decay` every `decay_step` steps till
-a minimum of `clip`.
-
-# Parameters
-- Learning rate (`η`): Amount by which gradients are discounted before updating
-                       the weights.
-- `decay`: Factor by which the learning rate is discounted.
-- `decay_step`: Schedule decay operations by setting the number of steps between
-                two decay operations.
-- `clip`: Minimum value of learning rate.
-- 'start': Step at which the decay starts.
-
-# Examples
-
-`ExpDecay` is typically composed  with other optimizers
-as the last transformation of the gradient:
-```julia
-opt = Optimiser(ADAM(), ExpDecay())
-```
-"""
+# Based on Flux.ExpDecay, but allowing a `start` parameter
 mutable struct ExpDecay <: Flux.Optimise.AbstractOptimiser
     eta::Float64
     decay::Float64
@@ -43,6 +21,7 @@ function Flux.Optimise.apply!(o::ExpDecay, x, Δ)
     return @. Δ *= o.eta
 end
 
+# Based on Flux.ADAM, but taking `ϵ` as parameter
 mutable struct ADAM <: Flux.Optimise.AbstractOptimiser
     eta::Float64
     beta::Tuple{Float64,Float64}
