e90c6697155ba64bbe5bccb060fbc2cf826d327a
cossio <j.cossio.diaz@gmail.com>
2022-01-27 14:45:16 +0100

    zerosum disambiguation for for Potts / Potts

--- src/zerosum.jl
+++ src/zerosum.jl
@@ -11,8 +11,8 @@ function zerosum!(rbm::RBM)
     return rbm
 end
 
-_zerosum_visible!(rbm::RBM) = rbm
-_zerosum_hidden!(rbm::RBM)  = rbm
+_zerosum_visible!(rbm::RBM{<:AbstractLayer, <:AbstractLayer}) = rbm
+_zerosum_hidden!(rbm::RBM{<:AbstractLayer, <:AbstractLayer})  = rbm
 
 function _zerosum_visible!(rbm::RBM{<:Potts, <:AbstractLayer})
     zerosum!(rbm.visible)
@@ -22,6 +22,14 @@ end
 
 function _zerosum_hidden!(rbm::RBM{<:AbstractLayer, <:Potts})
     zerosum!(rbm.hidden)
+    zerosum!(rbm.w; dims = 1 + ndims(rbm.visible))
+    return nothing
+end
+
+function zerosum!(rbm::RBM{<:Potts, <:Potts})
+    zerosum!(rbm.visible)
+    zerosum!(rbm.hidden)
+    zerosum!(rbm.w; dims = 1)
     zerosum!(rbm.w; dims = 1 + ndims(rbm.visible))
     return nothing
 end
