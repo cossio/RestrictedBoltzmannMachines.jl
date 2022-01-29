0f1f53e578f042a6d2cd261db0c5ce72b2bbd2e5
cossio <j.cossio.diaz@gmail.com>
2022-01-29 16:52:34 +0100

    allow passing initial fantasy chains to PCD save fantasy chains to history in PCD

--- src/train/pcd.jl
+++ src/train/pcd.jl
@@ -4,20 +4,23 @@
 Trains the RBM on data using Persistent Contrastive divergence.
 """
 function pcd!(rbm::RBM, data::AbstractArray;
-    batchsize::Int = 1,
-    epochs::Int = 1,
-    optimizer = default_optimizer(_nobs(data), batchsize, epochs), # optimizer algorithm
-    history::MVHistory = MVHistory(), # stores training log
-    wts = nothing, # data point weights
-    steps::Int = 1, # Monte Carlo steps to update fantasy particles
+    batchsize::Int = 1, # size of minibatches
+    epochs::Int = 1, # number of training epochs
+    # optimizer algorithm
+    optimizer = default_optimizer(_nobs(data), batchsize, epochs),
+    # stores data during training (pseudolikelihood, gradient norms, etc.)
+    history::MVHistory = MVHistory(),
+    # data point weights
+    wts = nothing,
+    # Monte Carlo steps to update fantasy particles
+    steps::Int = 1,
+    # initial state of fantasy chains (default: sample visible layer)
+    vm::AbstractArray = transfer_sample(rbm.visible, falses(size(rbm.visible)..., batchsize))
 )
     @assert size(data) == (size(rbm.visible)..., size(data)[end])
     @assert isnothing(wts) || _nobs(data) == _nobs(wts)
 
     stats = sufficient_statistics(rbm.visible, data; wts)
-
-    # initialize fantasy chains by sampling visible layer
-    vm = transfer_sample(rbm.visible, falses(size(rbm.visible)..., batchsize))
 
     for epoch in 1:epochs
         batches = minibatches(data, wts; batchsize = batchsize)
@@ -36,10 +39,12 @@ function pcd!(rbm::RBM, data::AbstractArray;
         push!(history, :lpl, lpl)
         push!(history, :epoch, epoch)
         push!(history, :Δt, Δt)
+        push!(history, :vm, copy(vm))
 
         Δt_ = round(Δt, digits=2)
         lpl_ = round(lpl, digits=2)
         @debug "epoch $epoch/$epochs ($(Δt_)s), log(PL)=$lpl_"
     end
+
     return history
 end
