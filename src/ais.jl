16c1c03fce968f9586cfefc89496e5adbd5626e1
cossio <j.cossio.diaz@gmail.com>
2022-01-29 11:59:33 +0100

    ais comment

--- src/ais.jl
+++ src/ais.jl
@@ -1,6 +1,6 @@
-# Normal AIS tends to ovestimate likelihood.
-# Use http://proceedings.mlr.press/v38/burda15.html to lower-bound the log-likelihood
-# together we have an interval of where log-likelihood lies.
-
-#= Make inputs_v_to_h, inputs_h_to_v, etc... accept batches in multiple dimensions.
-Useful here sice we put temperatures in one dimension and batches in another. =#
+#=
+Normal AIS tends to ovestimate likelihood. We can also use:
+http://proceedings.mlr.press/v38/burda15.html
+to lower-bound the log-likelihood.
+Using both, we have an interval where true log-likelihood lies.
+=#
