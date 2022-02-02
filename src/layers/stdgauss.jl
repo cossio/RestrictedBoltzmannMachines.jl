6b7fe7409e6f2d14ca547b7fb0d1054836c80bd3
cossio <j.cossio.diaz@gmail.com>
2022-02-02 16:08:07 +0100

    simplify StdGauss

--- src/layers/stdgauss.jl
+++ src/layers/stdgauss.jl
@@ -16,10 +16,10 @@ Base.size(layer::StdGauss) = layer.n
 Base.size(layer::StdGauss, d::Int) = layer.n[d]
 Base.length(layer::StdGauss) = prod(layer.n)
 
-function effective(layer::StdGauss{N,T}, inputs::AbstractArray; β::Real = true) where {N,T}
+function effective(layer::StdGauss, inputs::AbstractArray; β::Real = true)
     @assert size(layer) == size(inputs)[1:ndims(layer)]
-    θ = T(β) * inputs
-    γ = oftype(θ, FillArrays.Fill(β, size(inputs)))
+    θ = β * inputs
+    γ = oftype(θ, FillArrays.Fill(β, size(θ)))
     return Gaussian(θ, γ)
 end
 
@@ -28,13 +28,13 @@ function energies(layer::StdGauss, x::AbstractArray)
     return x.^2 / 2
 end
 
-free_energies(layer::StdGauss{N,T}) where {N,T} = FillArrays.Fill(-log(2T(π))/2, size(layer))
+free_energies(layer::StdGauss) = FillArrays.Fill(-log(2π)/2, size(layer))
 transfer_mode(layer::StdGauss) = FillArrays.Falses(size(layer))
 transfer_mean(layer::StdGauss) = transfer_mode(layer)
 transfer_var(layer::StdGauss) = FillArrays.Trues(size(layer))
 transfer_std(layer::StdGauss) = transfer_var(layer)  # √1 = 1
-transfer_mean_abs(layer::StdGauss{N,T}) where {N,T} = FillArrays.Fill(√(2/T(π)), size(layer))
-transfer_sample(layer::StdGauss{N,T}) where {N,T} = randn(T, size(layer))
+transfer_mean_abs(layer::StdGauss) = FillArrays.Fill(√(2/π), size(layer))
+transfer_sample(layer::StdGauss) = randn(size(layer)...)
 ∂free_energy(::StdGauss) = (;)
 ∂energy(::StdGauss) = (;)
 
