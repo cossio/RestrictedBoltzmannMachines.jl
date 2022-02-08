182d24b848199cdd54d8d601d62e791d35696a84
cossio <j.cossio.diaz@gmail.com>
2022-02-08 16:58:50 +0100

    deindent code within module definition

--- src/RestrictedBoltzmannMachines.jl
+++ src/RestrictedBoltzmannMachines.jl
@@ -1,50 +1,52 @@
 module RestrictedBoltzmannMachines
-    import Random
-    import Statistics
-    import LinearAlgebra
-    import SpecialFunctions
-    import LogExpFunctions
-    import FillArrays
-    import Flux
-    import Zygote
-    import ChainRulesCore
-    using ValueHistories: MVHistory
 
-    include("util.jl")
-    include("minibatches.jl")
-    include("onehot.jl")
-    include("linalg.jl")
-    include("truncnorm.jl")
+import Random
+import Statistics
+import LinearAlgebra
+import SpecialFunctions
+import LogExpFunctions
+import FillArrays
+import Flux
+import Zygote
+import ChainRulesCore
+using ValueHistories: MVHistory
 
-    include("layers/abstractlayer.jl")
-    include("layers/binary.jl")
-    include("layers/spin.jl")
-    include("layers/potts.jl")
-    include("layers/gaussian.jl")
-    include("layers/relu.jl")
-    include("layers/drelu.jl")
-    include("layers/prelu.jl")
-    include("layers/xrelu.jl")
-    include("layers/common.jl")
+include("util.jl")
+include("minibatches.jl")
+include("onehot.jl")
+include("linalg.jl")
+include("truncnorm.jl")
 
-    include("abstractrbm.jl")
-    include("rbm.jl")
+include("layers/abstractlayer.jl")
+include("layers/binary.jl")
+include("layers/spin.jl")
+include("layers/potts.jl")
+include("layers/gaussian.jl")
+include("layers/relu.jl")
+include("layers/drelu.jl")
+include("layers/prelu.jl")
+include("layers/xrelu.jl")
+include("layers/common.jl")
 
-    include("rbms/hopfield.jl")
-    include("rbms/binary.jl")
+include("abstractrbm.jl")
+include("rbm.jl")
 
-    include("zerosum.jl")
-    include("pseudolikelihood.jl")
-    include("partition.jl")
-    include("ais.jl")
+include("rbms/hopfield.jl")
+include("rbms/binary.jl")
 
-    include("train/initialization.jl")
-    include("train/cd.jl")
-    include("train/cdad.jl")
-    include("train/pcd.jl")
-    include("train/bnorm.jl")
-    include("train/fast.jl")
-    include("train/rdm.jl")
-    include("train/optim.jl")
-    include("train/regularize.jl")
-end
+include("zerosum.jl")
+include("pseudolikelihood.jl")
+include("partition.jl")
+include("ais.jl")
+
+include("train/initialization.jl")
+include("train/cd.jl")
+include("train/cdad.jl")
+include("train/pcd.jl")
+include("train/bnorm.jl")
+include("train/fast.jl")
+include("train/rdm.jl")
+include("train/optim.jl")
+include("train/regularize.jl")
+
+end # module
