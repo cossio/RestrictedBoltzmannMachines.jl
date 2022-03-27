a7e3d84486559ec4589a308190b615a3771025a6
cossio <j.cossio.diaz@gmail.com>
2022-03-27 15:47:45 +0200

    more using in tests

--- test/rbm.jl
+++ test/rbm.jl
@@ -9,52 +9,55 @@ using Statistics: mean
 using LinearAlgebra: logdet, diag, diagm, dot, isposdef
 using LogExpFunctions: logsumexp
 using QuadGK: quadgk
+using RestrictedBoltzmannMachines: visible, hidden, weights
+using RestrictedBoltzmannMachines: inputs_v_to_h, inputs_h_to_v
+using RestrictedBoltzmannMachines: energy, interaction_energy, free_energy
 
 @testset "batches, n=$n, m=$m, Bv=$Bv, Bh=$Bh" for n in (5, (5,2)), m in (2, (3,4)), Bv in ((), (3,2)), Bh in ((), (3,2))
     rbm = RBMs.BinaryRBM(randn(n...), randn(m...), randn(n..., m...))
-    wmat = reshape(rbm.w, length(rbm.visible), length(rbm.hidden))
+    wmat = reshape(weights(rbm), length(visible(rbm)), length(hidden(rbm)))
     v = bitrand(n..., Bv...)
     h = bitrand(m..., Bh...)
 
-    @test RBMs.batch_size(rbm.visible, v) == Bv
-    @test RBMs.batch_size(rbm.hidden, h) == Bh
+    @test RBMs.batch_size(visible(rbm), v) == Bv
+    @test RBMs.batch_size(hidden(rbm), h) == Bh
 
-    @test size(RBMs.inputs_v_to_h(rbm, v)) == (size(rbm.hidden)...,  Bv...)
-    @test size(RBMs.inputs_h_to_v(rbm, h)) == (size(rbm.visible)..., Bh...)
+    @test size(inputs_v_to_h(rbm, v)) == (size(hidden(rbm))...,  Bv...)
+    @test size(inputs_h_to_v(rbm, h)) == (size(visible(rbm))..., Bh...)
 
     if length(Bv) == length(Bh) == 0
-        @test RBMs.interaction_energy(rbm, v, h) isa Number
-        @test RBMs.interaction_energy(rbm, v, h) ≈ -vec(v)' * wmat * vec(h)
-        @test RBMs.energy(rbm, v, h) isa Number
+        @test interaction_energy(rbm, v, h) isa Number
+        @test interaction_energy(rbm, v, h) ≈ -vec(v)' * wmat * vec(h)
+        @test energy(rbm, v, h) isa Number
     elseif length(Bv) == 0
-        hmat = reshape(h, length(rbm.hidden), :)
+        hmat = reshape(h, length(hidden(rbm)), :)
         E = -vec(v)' * wmat * hmat
-        @test RBMs.interaction_energy(rbm, v, h) isa AbstractArray
-        @test size(RBMs.interaction_energy(rbm, v, h)) == Bh
-        @test RBMs.interaction_energy(rbm, v, h) ≈ reshape(E, Bh)
-        @test size(RBMs.energy(rbm, v, h)) == Bh
+        @test interaction_energy(rbm, v, h) isa AbstractArray
+        @test size(interaction_energy(rbm, v, h)) == Bh
+        @test interaction_energy(rbm, v, h) ≈ reshape(E, Bh)
+        @test size(energy(rbm, v, h)) == Bh
     elseif length(Bh) == 0
-        vmat = reshape(v, length(rbm.visible), :)
+        vmat = reshape(v, length(visible(rbm)), :)
         E = -vec(h)' * wmat' * vmat
-        @test RBMs.interaction_energy(rbm, v, h) isa AbstractArray
-        @test size(RBMs.interaction_energy(rbm, v, h)) == Bv
-        @test RBMs.interaction_energy(rbm, v, h) ≈ reshape(E, Bv)
-        @test size(RBMs.energy(rbm, v, h)) == Bv
+        @test interaction_energy(rbm, v, h) isa AbstractArray
+        @test size(interaction_energy(rbm, v, h)) == Bv
+        @test interaction_energy(rbm, v, h) ≈ reshape(E, Bv)
+        @test size(energy(rbm, v, h)) == Bv
     else
-        vmat = reshape(v, length(rbm.visible), :)
-        hmat = reshape(h, length(rbm.hidden), :)
+        vmat = reshape(v, length(visible(rbm)), :)
+        hmat = reshape(h, length(hidden(rbm)), :)
         E = -dot.(eachcol(vmat), Ref(wmat), eachcol(hmat))
-        @test RBMs.interaction_energy(rbm, v, h) isa AbstractArray
-        @test size(RBMs.interaction_energy(rbm, v, h)) == Bv == Bh
-        @test RBMs.interaction_energy(rbm, v, h) ≈ reshape(E, Bv)
-        @test size(RBMs.energy(rbm, v, h)) == Bv == Bh
+        @test interaction_energy(rbm, v, h) isa AbstractArray
+        @test size(interaction_energy(rbm, v, h)) == Bv == Bh
+        @test interaction_energy(rbm, v, h) ≈ reshape(E, Bv)
+        @test size(energy(rbm, v, h)) == Bv == Bh
     end
-    @inferred RBMs.inputs_v_to_h(rbm, v)
-    @inferred RBMs.inputs_h_to_v(rbm, h)
-    @inferred RBMs.interaction_energy(rbm, v, h)
-    @inferred RBMs.energy(rbm, v, h)
+    @inferred inputs_v_to_h(rbm, v)
+    @inferred inputs_h_to_v(rbm, h)
+    @inferred interaction_energy(rbm, v, h)
+    @inferred energy(rbm, v, h)
     gs = Zygote.gradient(rbm) do rbm
-        mean(RBMs.energy(rbm, v, h))
+        mean(energy(rbm, v, h))
     end
     ∂w = @inferred RBMs.∂interaction_energy(rbm, v, h)
     @test ∂w ≈ only(gs).w
@@ -65,151 +68,151 @@ end
 
     v = bitrand(3,1,2)
     h = bitrand(2,3,1)
-    @test size(RBMs.energy(rbm, v, h)) == @inferred(RBMs.batch_size(rbm, v, h)) == (3,2)
-    @test RBMs.energy(rbm, v, h) ≈ [RBMs.energy(rbm, v[:,1,j], h[:,i,1]) for i = 1:3, j = 1:2]
+    @test size(energy(rbm, v, h)) == @inferred(RBMs.batch_size(rbm, v, h)) == (3,2)
+    @test energy(rbm, v, h) ≈ [energy(rbm, v[:,1,j], h[:,i,1]) for i = 1:3, j = 1:2]
 
     v = bitrand(3,1,2)
     h = bitrand(2,3)
-    @test size(RBMs.energy(rbm, v, h)) == @inferred(RBMs.batch_size(rbm, v, h)) == (3,2)
-    @test RBMs.energy(rbm, v, h) ≈ [RBMs.energy(rbm, v[:,1,j], h[:,i]) for i = 1:3, j = 1:2]
+    @test size(energy(rbm, v, h)) == @inferred(RBMs.batch_size(rbm, v, h)) == (3,2)
+    @test energy(rbm, v, h) ≈ [energy(rbm, v[:,1,j], h[:,i]) for i = 1:3, j = 1:2]
 
     v = bitrand(3,1)
     h = bitrand(2,3,2)
-    @test size(RBMs.energy(rbm, v, h)) == @inferred(RBMs.batch_size(rbm, v, h)) == (3,2)
-    @test RBMs.energy(rbm, v, h) ≈ [RBMs.energy(rbm, v[:,1], h[:,i,j]) for i = 1:3, j = 1:2]
+    @test size(energy(rbm, v, h)) == @inferred(RBMs.batch_size(rbm, v, h)) == (3,2)
+    @test energy(rbm, v, h) ≈ [energy(rbm, v[:,1], h[:,i,j]) for i = 1:3, j = 1:2]
 
     v = bitrand(3,1,2)
     h = bitrand(2,3,2)
-    @test size(RBMs.energy(rbm, v, h)) == @inferred(RBMs.batch_size(rbm, v, h)) == (3,2)
-    @test RBMs.energy(rbm, v, h) ≈ [RBMs.energy(rbm, v[:,1,j], h[:,i,j]) for i = 1:3, j = 1:2]
+    @test size(energy(rbm, v, h)) == @inferred(RBMs.batch_size(rbm, v, h)) == (3,2)
+    @test energy(rbm, v, h) ≈ [energy(rbm, v[:,1,j], h[:,i,j]) for i = 1:3, j = 1:2]
 
     v = bitrand(3,3,1)
     h = bitrand(2,3,2)
-    @test size(RBMs.energy(rbm, v, h)) == @inferred(RBMs.batch_size(rbm, v, h)) == (3,2)
-    @test RBMs.energy(rbm, v, h) ≈ [RBMs.energy(rbm, v[:,i,1], h[:,i,j]) for i = 1:3, j = 1:2]
+    @test size(energy(rbm, v, h)) == @inferred(RBMs.batch_size(rbm, v, h)) == (3,2)
+    @test energy(rbm, v, h) ≈ [energy(rbm, v[:,i,1], h[:,i,j]) for i = 1:3, j = 1:2]
 
     v = bitrand(3,2,2)
     h = bitrand(2,3,1)
     @test_throws Any RBMs.batch_size(rbm, v, h)
-    @test_throws Any RBMs.energy(rbm, v, h)
+    @test_throws Any energy(rbm, v, h)
 
     v = bitrand(3,1,2)
     h = bitrand(2,3,3)
     @test_throws Any RBMs.batch_size(rbm, v, h)
-    @test_throws Any RBMs.energy(rbm, v, h)
+    @test_throws Any energy(rbm, v, h)
 end
 
 @testset "sample_v_from_v and sample_h_from_h on binary RBM" begin
     rbm = RBMs.BinaryRBM(randn(3,2), randn(2,3), zeros(3,2,2,3))
-    v = bitrand(size(rbm.visible)..., 10^6)
+    v = bitrand(size(visible(rbm))..., 10^6)
     v = RBMs.sample_v_from_v(rbm, v)
-    @test RBMs.batchmean(rbm.visible, v) ≈ RBMs.transfer_mean(rbm.visible) rtol=0.1
+    @test RBMs.batchmean(visible(rbm), v) ≈ RBMs.transfer_mean(visible(rbm)) rtol=0.1
 
-    h = bitrand(size(rbm.hidden)...,  10^6)
+    h = bitrand(size(hidden(rbm))...,  10^6)
     h = RBMs.sample_h_from_h(rbm, h)
-    @test RBMs.batchmean(rbm.hidden, h) ≈ RBMs.transfer_mean(rbm.hidden) rtol=0.1
+    @test RBMs.batchmean(hidden(rbm), h) ≈ RBMs.transfer_mean(hidden(rbm)) rtol=0.1
 
-    randn!(rbm.w)
+    randn!(weights(rbm))
     h = RBMs.sample_h_from_v(rbm, v)
-    μ = RBMs.transfer_mean(rbm.hidden, RBMs.inputs_v_to_h(rbm, v))
-    @test RBMs.batchmean(rbm.hidden, h) ≈ RBMs.batchmean(rbm.hidden, μ) rtol=0.1
+    μ = RBMs.transfer_mean(hidden(rbm), inputs_v_to_h(rbm, v))
+    @test RBMs.batchmean(hidden(rbm), h) ≈ RBMs.batchmean(hidden(rbm), μ) rtol=0.1
 
-    randn!(rbm.w)
+    randn!(weights(rbm))
     v = RBMs.sample_v_from_h(rbm, h)
-    μ = RBMs.transfer_mean(rbm.visible, RBMs.inputs_h_to_v(rbm, h))
-    @test RBMs.batchmean(rbm.visible, v) ≈ RBMs.batchmean(rbm.visible, μ) rtol=0.1
+    μ = RBMs.transfer_mean(visible(rbm), inputs_h_to_v(rbm, h))
+    @test RBMs.batchmean(visible(rbm), v) ≈ RBMs.batchmean(visible(rbm), μ) rtol=0.1
 end
 
 @testset "rbm convenience constructors" begin
     rbm = RBMs.BinaryRBM(randn(5), randn(3), randn(5,3))
-    @test rbm.visible isa RBMs.Binary
-    @test rbm.hidden isa RBMs.Binary
-    @test size(rbm.w) == (5,3)
+    @test visible(rbm) isa RBMs.Binary
+    @test hidden(rbm) isa RBMs.Binary
+    @test size(weights(rbm)) == (5,3)
 
     rbm = RBMs.HopfieldRBM(randn(5), randn(3), rand(3), randn(5,3))
-    @test rbm.visible isa RBMs.Spin
-    @test rbm.hidden isa RBMs.Gaussian
+    @test visible(rbm) isa RBMs.Spin
+    @test hidden(rbm) isa RBMs.Gaussian
     @test size(RBMs.weights(rbm)) == (5,3)
-    @test all(rbm.hidden.γ .> 0)
+    @test all(hidden(rbm).γ .> 0)
 
     rbm = RBMs.HopfieldRBM(randn(5), randn(5,3))
     @test RBMs.visible(rbm) isa RBMs.Spin
-    @test iszero(RBMs.transfer_mean(RBMs.hidden(rbm)))
-    @test iszero(RBMs.transfer_var(RBMs.hidden(rbm)) .- 1)
+    @test iszero(RBMs.transfer_mean(hidden(rbm)))
+    @test RBMs.transfer_var(hidden(rbm)) == ones(size(hidden(rbm)))
     @test size(RBMs.weights(rbm)) == (5,3)
 end
 
 @testset "Binary-Binary RBM" begin
     rbm = RBMs.BinaryRBM(randn(5,2), randn(4,3), randn(5,2,4,3))
-    v = rand(Bool, size(rbm.visible)..., 7)
-    h = rand(Bool, size(rbm.hidden)..., 7)
-    @test size(@inferred RBMs.interaction_energy(rbm, v, h)) == (7,)
-    @test size(@inferred RBMs.energy(rbm, v, h)) == (7,)
+    v = rand(Bool, size(visible(rbm))..., 7)
+    h = rand(Bool, size(hidden(rbm))..., 7)
+    @test size(@inferred interaction_energy(rbm, v, h)) == (7,)
+    @test size(@inferred energy(rbm, v, h)) == (7,)
 
-    Ew = -[sum(v[i,j,b] * rbm.w[i,j,μ,ν] * h[μ,ν,b] for i=1:5, j=1:2, μ=1:4, ν=1:3) for b=1:7]
-    @test RBMs.interaction_energy(rbm, v, h) ≈ Ew
-    @test RBMs.energy(rbm, v, h) ≈ RBMs.energy(rbm.visible, v) + RBMs.energy(rbm.hidden, h) + Ew
+    Ew = -[sum(v[i,j,b] * weights(rbm)[i,j,μ,ν] * h[μ,ν,b] for i=1:5, j=1:2, μ=1:4, ν=1:3) for b=1:7]
+    @test interaction_energy(rbm, v, h) ≈ Ew
+    @test energy(rbm, v, h) ≈ energy(visible(rbm), v) + energy(hidden(rbm), h) + Ew
 
-    @test (@inferred RBMs.energy(rbm, v[:,:,1], h[:,:,1])) isa Real
-    @test (@inferred RBMs.energy(rbm, v[:,:,1], h)) isa AbstractVector{<:Real}
-    @test (@inferred RBMs.energy(rbm, v, h[:,:,1])) isa AbstractVector{<:Real}
+    @test (@inferred energy(rbm, v[:,:,1], h[:,:,1])) isa Real
+    @test (@inferred energy(rbm, v[:,:,1], h)) isa AbstractVector{<:Real}
+    @test (@inferred energy(rbm, v, h[:,:,1])) isa AbstractVector{<:Real}
 
     for b = 1:7
-        @test RBMs.energy(rbm, v[:,:,b], h[:,:,b]) ≈ RBMs.energy(rbm, v, h)[b]
-        @test RBMs.energy(rbm, v[:,:,b], h) ≈ [RBMs.energy(rbm, v[:,:,b], h[:,:,k]) for k=1:7]
-        @test RBMs.energy(rbm, v, h[:,:,b]) ≈ [RBMs.energy(rbm, v[:,:,k], h[:,:,b]) for k=1:7]
+        @test energy(rbm, v[:,:,b], h[:,:,b]) ≈ energy(rbm, v, h)[b]
+        @test energy(rbm, v[:,:,b], h) ≈ [energy(rbm, v[:,:,b], h[:,:,k]) for k=1:7]
+        @test energy(rbm, v, h[:,:,b]) ≈ [energy(rbm, v[:,:,k], h[:,:,b]) for k=1:7]
     end
 
     @test size(@inferred RBMs.sample_h_from_v(rbm, v)) == size(h)
     @test size(@inferred RBMs.sample_v_from_h(rbm, h)) == size(v)
-    @test size(@inferred RBMs.sample_h_from_v(rbm, v[:,:,1])) == size(rbm.hidden)
-    @test size(@inferred RBMs.sample_v_from_h(rbm, h[:,:,1])) == size(rbm.visible)
+    @test size(@inferred RBMs.sample_h_from_v(rbm, v[:,:,1])) == size(hidden(rbm))
+    @test size(@inferred RBMs.sample_v_from_h(rbm, h[:,:,1])) == size(visible(rbm))
     for k = 1:3
         @test size(@inferred RBMs.sample_v_from_v(rbm, v; steps=k)) == size(v)
         @test size(@inferred RBMs.sample_h_from_h(rbm, h; steps=k)) == size(h)
-        @test size(@inferred RBMs.sample_v_from_v(rbm, v[:,:,1]; steps=k)) == size(rbm.visible)
-        @test size(@inferred RBMs.sample_h_from_h(rbm, h[:,:,1]; steps=k)) == size(rbm.hidden)
+        @test size(@inferred RBMs.sample_v_from_v(rbm, v[:,:,1]; steps=k)) == size(visible(rbm))
+        @test size(@inferred RBMs.sample_h_from_h(rbm, h[:,:,1]; steps=k)) == size(hidden(rbm))
     end
 
     @test size(@inferred RBMs.mean_h_from_v(rbm, v)) == size(h)
     @test size(@inferred RBMs.mean_v_from_h(rbm, h)) == size(v)
-    @test size(@inferred RBMs.mean_h_from_v(rbm, v[:,:,1])) == size(rbm.hidden)
-    @test size(@inferred RBMs.mean_v_from_h(rbm, h[:,:,1])) == size(rbm.visible)
+    @test size(@inferred RBMs.mean_h_from_v(rbm, v[:,:,1])) == size(hidden(rbm))
+    @test size(@inferred RBMs.mean_v_from_h(rbm, h[:,:,1])) == size(visible(rbm))
 
     @test size(@inferred RBMs.mode_h_from_v(rbm, v)) == size(h)
     @test size(@inferred RBMs.mode_v_from_h(rbm, h)) == size(v)
-    @test size(@inferred RBMs.mode_h_from_v(rbm, v[:,:,1])) == size(rbm.hidden)
-    @test size(@inferred RBMs.mode_v_from_h(rbm, h[:,:,1])) == size(rbm.visible)
+    @test size(@inferred RBMs.mode_h_from_v(rbm, v[:,:,1])) == size(hidden(rbm))
+    @test size(@inferred RBMs.mode_v_from_h(rbm, h[:,:,1])) == size(visible(rbm))
 
     @test size(@inferred RBMs.var_h_from_v(rbm, v)) == size(h)
     @test size(@inferred RBMs.var_v_from_h(rbm, h)) == size(v)
-    @test size(@inferred RBMs.var_h_from_v(rbm, v[:,:,1])) == size(rbm.hidden)
-    @test size(@inferred RBMs.var_v_from_h(rbm, h[:,:,1])) == size(rbm.visible)
+    @test size(@inferred RBMs.var_h_from_v(rbm, v[:,:,1])) == size(hidden(rbm))
+    @test size(@inferred RBMs.var_v_from_h(rbm, h[:,:,1])) == size(visible(rbm))
 
-    @test size(@inferred RBMs.free_energy(rbm, v)) == (7,)
+    @test size(@inferred free_energy(rbm, v)) == (7,)
     @test size(@inferred RBMs.reconstruction_error(rbm, v)) == (7,)
-    @test (@inferred RBMs.free_energy(rbm, v[:,:,1])) isa Real
+    @test (@inferred free_energy(rbm, v[:,:,1])) isa Real
     @test (@inferred RBMs.reconstruction_error(rbm, v[:,:,1])) isa Real
 
     @inferred RBMs.mirror(rbm)
-    @test RBMs.mirror(rbm).visible == rbm.hidden
-    @test RBMs.mirror(rbm).hidden == rbm.visible
-    @test RBMs.energy(RBMs.mirror(rbm), h, v) ≈ RBMs.energy(rbm, v, h)
+    @test RBMs.mirror(rbm).visible == hidden(rbm)
+    @test RBMs.mirror(rbm).hidden == visible(rbm)
+    @test energy(RBMs.mirror(rbm), h, v) ≈ energy(rbm, v, h)
 end
 
 @testset "binary free energy" begin
     β = π
     rbm = RBMs.BinaryRBM(randn(1), randn(1), randn(1,1))
     for v in [[0], [1]]
-        @test -RBMs.free_energy(rbm, v) ≈ logsumexp(-RBMs.energy(rbm, v, h) for h in [[0], [1]])
-        @test -β * RBMs.free_energy(rbm, v; β) ≈ logsumexp(-β * RBMs.energy(rbm, v, h) for h in [[0], [1]])
+        @test -free_energy(rbm, v) ≈ logsumexp(-energy(rbm, v, h) for h in [[0], [1]])
+        @test -β * free_energy(rbm, v; β) ≈ logsumexp(-β * energy(rbm, v, h) for h in [[0], [1]])
     end
 
     rbm = RBMs.BinaryRBM(randn(7), randn(2), randn(7,2))
     v = bitrand(7)
     hs = [[0,0], [0,1], [1,0], [1,1]]
-    @test -RBMs.free_energy(rbm, v) ≈ logsumexp(-RBMs.energy(rbm, v, h) for h in hs)
-    @test -β * RBMs.free_energy(rbm, v; β) ≈ logsumexp(-β * RBMs.energy(rbm, v, h) for h in hs)
+    @test -free_energy(rbm, v) ≈ logsumexp(-energy(rbm, v, h) for h in hs)
+    @test -β * free_energy(rbm, v; β) ≈ logsumexp(-β * energy(rbm, v, h) for h in hs)
 end
 
 @testset "Gaussian-Gaussian RBM, 1-dimension" begin
@@ -220,17 +223,17 @@ end
     )
 
     @assert isposdef([
-        rbm.visible.γ -rbm.w;
-        -rbm.w'  rbm.hidden.γ
+        visible(rbm).γ -weights(rbm);
+        -weights(rbm)'  hidden(rbm).γ
     ])
 
     @test RBMs.log_partition(rbm; β = 1) ≈ RBMs.log_partition(rbm)
 
-    Z, ϵ = quadgk(x -> exp(-only(RBMs.free_energy(rbm, [x;;]))), -Inf, Inf)
+    Z, ϵ = quadgk(x -> exp(-only(free_energy(rbm, [x;;]))), -Inf, Inf)
     @test RBMs.log_partition(rbm) ≈ log(Z)
 
     β = 1.5
-    Z, ϵ = quadgk(x -> exp(-β * only(RBMs.free_energy(rbm, [x;;]; β))), -Inf, Inf)
+    Z, ϵ = quadgk(x -> exp(-β * only(free_energy(rbm, [x;;]; β))), -Inf, Inf)
     @test RBMs.log_partition(rbm; β) ≈ log(Z)
 end
 
@@ -243,16 +246,16 @@ end
         randn(n..., m...) / (10 * prod(n) * prod(m))
     )
 
-    N = length(rbm.visible)
-    M = length(rbm.hidden)
+    N = length(visible(rbm))
+    M = length(hidden(rbm))
 
     θ = [
-        vec(rbm.visible.θ);
-        vec(rbm.hidden.θ)
+        vec(visible(rbm).θ);
+        vec(hidden(rbm).θ)
     ]
-    γv = vec(rbm.visible.γ)
-    γh = vec(rbm.hidden.γ)
-    w = reshape(rbm.w, length(rbm.visible), length(rbm.hidden))
+    γv = vec(visible(rbm).γ)
+    γh = vec(hidden(rbm).γ)
+    w = reshape(weights(rbm), length(visible(rbm)), length(hidden(rbm)))
     A = [diagm(γv) -w;
          -w'  diagm(γh)]
 
@@ -260,7 +263,7 @@ end
     h = randn(m..., 1)
     x = [reshape(v, N, 1); reshape(h, M, 1)]
 
-    @test RBMs.energy(rbm, v, h) ≈ x' * A * x / 2 - θ' * x
+    @test energy(rbm, v, h) ≈ x' * A * x / 2 - θ' * x
     @test RBMs.log_partition(rbm) ≈ (
         (N + M)/2 * log(2π) + θ' * inv(A) * θ / 2 - logdet(A)/2
     )
@@ -273,12 +276,12 @@ end
 
     @test RBMs.log_likelihood(rbm, v; β = 1) ≈ RBMs.log_likelihood(rbm, v)
     @test RBMs.log_likelihood(rbm, v; β) ≈ (
-        -β * RBMs.free_energy(rbm, v; β) .- RBMs.log_partition(rbm; β)
+        -β * free_energy(rbm, v; β) .- RBMs.log_partition(rbm; β)
     )
 
-    Ev = sum(@. rbm.visible.γ * v^2 / 2 - rbm.visible.θ * v)
-    Fv = sum(-(rbm.hidden.θ .+ RBMs.inputs_v_to_h(rbm, v)).^2 ./ 2rbm.hidden.γ)
-    @test only(RBMs.free_energy(rbm, v)) ≈ Ev + Fv - sum(log.(2π ./ rbm.hidden.γ)) / 2
+    Ev = sum(visible(rbm).γ .* v.^2 ./ 2 - visible(rbm).θ .* v)
+    Fv = sum(-(hidden(rbm).θ .+ inputs_v_to_h(rbm, v)).^2 ./ 2hidden(rbm).γ)
+    @test only(free_energy(rbm, v)) ≈ Ev + Fv - sum(log.(2π ./ hidden(rbm).γ)) / 2
 
     gs = Zygote.gradient(rbm) do rbm
         RBMs.log_partition(rbm)
@@ -287,7 +290,7 @@ end
     ∂θh = vec(only(gs).hidden.θ)
     ∂γv = vec(only(gs).visible.γ)
     ∂γh = vec(only(gs).hidden.γ)
-    ∂w = reshape(only(gs).w, length(rbm.visible), length(rbm.hidden))
+    ∂w = reshape(only(gs).w, length(visible(rbm)), length(hidden(rbm)))
 
     Σ = inv(A) # covariances
     μ = Σ * θ  # means
