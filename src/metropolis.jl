453394a8c83c95390e95b2496232d90fe97fef2a
cossio <j.cossio.diaz@gmail.com>
2022-05-01 17:28:09 +0200

    metropolis, cold_metropolis

--- src/metropolis.jl
+++ src/metropolis.jl
@@ -1,4 +1,33 @@
 """
+    metropolis(rbm, v; β = 1, steps = 1)
+
+Metropolis-Hastings sampling from `rbm` at inverse temperature β, starting from
+configuration `v`.
+"""
+function metropolis(rbm::RBM, v::AbstractArray; β::Real = 1, steps::Int = 1)
+    @assert size(v)[1:ndims(visible(rbm))] == size(visible(rbm))
+    for _ in 1:steps
+        v = oftype(v, metropolis_once(rbm, v; β))
+    end
+    return v
+end
+
+function metropolis_once(rbm::RBM, v::AbstractArray; β::Real = 1)
+    @assert size(v)[1:ndims(visible(rbm))] == size(visible(rbm))
+    v_new = sample_v_from_v_once(rbm, v)
+    ΔE = (β - 1) * (free_energy(rbm, v_new) - free_energy(rbm, v))
+    ℐ = CartesianIndices(size(visible(rbm))) # index span of visible layer
+    for n in CartesianIndices(ΔE)
+        if ΔE[n] ≤ 0 || randexp() > ΔE[n]
+            continue # accept move
+        else
+            v_new[ℐ, n] .= v[ℐ, n] # do not accept move
+        end
+    end
+    return v_new
+end
+
+"""
     metropolis!(v, rbm; β = 1)
 
 Metropolis-Hastings sampling from `rbm` at inverse temperature β.
@@ -6,23 +35,29 @@ Uses `v[:,:,..,:,1]` as initial configurations, and writes the
 Monte-Carlo chains in `v[:,:,..,:,2:end]`.
 """
 function metropolis!(v::AbstractArray, rbm::RBM; β::Real = 1)
-    nchains, nsteps = size(v)[end - 1], size(v)[end]
-    @assert size(v) == (size(visible(rbm))..., nchains, nsteps)
-    ℐ = CartesianIndices(size(visible(rbm))) # index span of visible layer
-    for t in 2:nsteps
-        v_old = selectdim(v, ndims(v), t - 1)
-        v_new = sample_v_from_v_once(rbm, v_old)
-        ΔE = (β - 1) * (free_energy(rbm, v_new) - free_energy(rbm, v_old))
-        @assert length(ΔE) == nchains
-        for n in 1:nchains
-            if ΔE[n] ≤ 0 || randexp() > ΔE[n]
-                # accept move
-                v[ℐ, n, t] .= v_new[ℐ, n]
-            else
-                # do not accept move
-                v[ℐ, n, t] .= v_old[ℐ, n]
-            end
-        end
+    @assert ndims(v) > ndims(visible(rbm)) # last dim contains nsteps
+    @assert size(v)[1:ndims(visible(rbm))] == size(visible(rbm))
+    for t in 2:size(v, ndims(v))
+        selectdim(v, ndims(v), t) .= metropolis_once(rbm, selectdim(v, ndims(v), t - 1); β)
     end
     return v
+end
+
+"""
+    cold_metropolis(rbm, v; steps = 1)
+
+Samples the `rbm` at zero temperature, starting from configuration `v`.
+"""
+function cold_metropolis(rbm::RBM, v::AbstractArray; steps::Int = 1)
+    @assert size(v)[1:ndims(visible(rbm))] == size(visible(rbm))
+    for _ in 1:steps
+        v = oftype(v, cold_metropolis_once(rbm, v))
+    end
+    return v
+end
+
+function cold_metropolis_once(rbm::RBM, v::AbstractArray)
+    @assert size(v)[1:ndims(visible(rbm))] == size(visible(rbm))
+    h = mean_h_from_v(rbm, v)
+    return mode_v_from_h(rbm, h)
 end
--- test/metropolis.jl
+++ test/metropolis.jl
@@ -2,8 +2,33 @@ using Test: @test, @testset, @inferred
 using Statistics: mean, std, var, cor
 using Random: randn!, bitrand
 using LogExpFunctions: logsumexp
-using RestrictedBoltzmannMachines: BinaryRBM, energy, free_energy, metropolis!
+using RestrictedBoltzmannMachines: BinaryRBM, energy, free_energy, metropolis, metropolis!
 import RestrictedBoltzmannMachines as RBMs
+
+@testset "metropolis" begin
+    N = 5
+    M = 2
+    rbm = BinaryRBM(randn(N), randn(M), randn(N, M) / √N)
+
+    β = 2
+    T = 10000
+    B = 100
+    v = bitrand(N, B, T)
+    for t in 2:T
+        v[:,:,t] .= metropolis(rbm, v[:,:,t - 1]; β)
+    end
+
+    counts = Dict{BitVector, Int}()
+    for t in 1000:T, n in 1:B
+        counts[v[:,n,t]] = get(counts, v[:,n,t], 0) + 1
+    end
+    freqs = Dict(v => c / sum(values(counts)) for (v,c) in counts)
+
+    𝒱 = [BitVector(digits(Bool, x; base=2, pad=N)) for x in 0:(2^N - 1)]
+    ℱ = free_energy.(Ref(rbm), 𝒱)
+
+    @test cor([get(freqs, v, 0.0) for v in 𝒱], exp.(-β * ℱ .- logsumexp(-β * ℱ))) > 0.99
+end
 
 @testset "metropolis!" begin
     N = 5
@@ -15,7 +40,6 @@ import RestrictedBoltzmannMachines as RBMs
     B = 100
     v = bitrand(N, B, T)
     metropolis!(v, rbm; β)
-
 
     counts = Dict{BitVector, Int}()
     for t in 1000:T, n in 1:B
