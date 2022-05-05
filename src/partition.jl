3acb5110642ab90b8e14f6d3e1d7c2d7dfb85ded
cossio <j.cossio.diaz@gmail.com>
2022-05-05 21:43:12 +0200

    avoid splatting in collect_states

--- src/partition.jl
+++ src/partition.jl
@@ -72,5 +72,7 @@ Only defined for discrete layers.
     For large layers, the exponential number of states will not fit in memory.
 """
 function collect_states(layer::Union{Binary, Spin, Potts})
-    return cat(iterate_states(layer)...; dims=ndims(layer) + 1)
+    reduce(iterate_states(layer)) do x, y
+        cat(x, y; dims = ndims(layer) + 1)
+    end
 end
