ed322f43c03cf38a6cf06a2b622f05eec3e284cf
Documenter.jl <documenter@juliadocs.github.io>
2022-05-09 21:40:04 +0000

    build based on 3f47d26

--- dev/literate/moment_match.jl
+++ dev/literate/moment_match.jl
@@ -3,15 +3,18 @@
 
 The stationarity conditions for ML learning of the RBM imply certain moment-matching
 conditions between the data and the RBM distribution.
+However, due to biased nature of the PCD approximation, these conditions might not hold
+exactly in practice.
 =#
 
 import MKL # faster on Intel CPUs
 import Makie, CairoMakie
 import MLDatasets
 import Flux
+using Statistics: mean, cor
+using LinearAlgebra: norm
 using RestrictedBoltzmannMachines: RBM, Binary, transfer_sample, free_energy
 using RestrictedBoltzmannMachines: sample_v_from_v, sample_h_from_v, initialize!, pcd!
-using Statistics: mean
 
 # load MNIST dataset
 
@@ -20,9 +23,11 @@ train_y = MLDatasets.MNIST(split=:train).targets;
 train_x = train_x[:, :, train_y .== 0]; # only zeros for speed
 
 # floating type we will use
+
 Float = Float32
 
 # compute training epochs from desired number of parameter updates
+
 function train_nepochs(;
     nsamples::Int, # number observations in the data
     nupdates::Int, # desired number of parameter updates
@@ -31,26 +36,35 @@ function train_nepochs(;
     return ceil(Int, nupdates * batchsize / nsamples)
 end
 
-# initialize RBM with 100 hidden units
-batchsize = 100
-nupdates = 1000
+# initialize RBM
+
+nhidden = 64
+batchsize = 64
+nupdates = 10000
 epochs = train_nepochs(; nsamples = size(train_x, 3), nupdates, batchsize)
-rbm = RBM(Binary(Float,28,28), Binary(Float,64), zeros(Float,28,28,64))
+rbm = RBM(Binary(Float,28,28), Binary(Float,nhidden), zeros(Float,28,28,nhidden))
 initialize!(rbm, train_x);
-optim = Flux.ADADelta()
-@time pcd!(rbm, train_x; epochs, batchsize, optim);
+@time pcd!(rbm, train_x; epochs, batchsize);
 
 # Generate samples, in v-space and h-space.
 
-nsteps = 200
+nsteps = 2000
 nsamples = 1000
-samples_v = falses(28, 28, nsamples, nsteps);
-samples_v[:,:,:,1] .= transfer_sample(rbm.visible, falses(28,28,nsamples));
-@time for t in 2:nsteps
-    samples_v[:, :, :, t] .= sample_v_from_v(rbm, samples_v[:, :, :, t - 1])
+v_model = falses(28, 28, nsamples, nsteps);
+v_model[:,:,:,1] .= transfer_sample(rbm.visible, falses(28,28,nsamples));
+for t in 2:nsteps
+    v_model[:, :, :, t] .= sample_v_from_v(rbm, v_model[:, :, :, t - 1])
 end
-data_h = sample_h_from_v(rbm, train_x);
-samples_h = sample_h_from_v(rbm, samples_v[:,:,:,end]);
+h_data = sample_h_from_v(rbm, train_x);
+h_model = sample_h_from_v(rbm, v_model[:,:,:,end]);
+
+v_data_center = train_x .- mean(train_x; dims=3);
+h_data_center = h_data .- mean(h_data; dims=2);
+vh_data = reshape(v_data_center, 28*28, :) * h_data_center' / size(v_data_center, 3);
+
+v_model_center = v_model[:,:,:,end] .- mean(v_model[:,:,:,end]; dims=3);
+h_model_center = h_model .- mean(h_model; dims=2);
+vh_model = reshape(v_model_center, 28*28, :) * h_model_center' / size(v_model_center, 3);
 
 # Plot moment-matching conditions
 
@@ -58,7 +72,7 @@ samples_h = sample_h_from_v(rbm, samples_v[:,:,:,end]);
 
 Makie.scatter(
     vec(mean(train_x; dims=3)),
-    vec(mean(samples_v[:,:,:,end]; dims=3))
+    vec(mean(v_model[:,:,:,end]; dims=3))
 )
 Makie.xlims!(0,1)
 Makie.ylims!(0,1)
@@ -67,8 +81,8 @@ Makie.current_figure()
 # <h>_data vs. <h>_model
 
 Makie.scatter(
-    vec(mean(data_h; dims=2)),
-    vec(mean(samples_h; dims=2)),
+    vec(mean(h_data; dims=2)),
+    vec(mean(h_model; dims=2)),
 )
 Makie.xlims!(0,1)
 Makie.ylims!(0,1)
@@ -76,26 +90,41 @@ Makie.current_figure()
 
 # <vh>_data vs. <vh>_model
 
-Makie.scatter(
-    vec(reshape(train_x, 28*28, :) * data_h' / size(train_x, 3)),
-    vec(reshape(samples_v[:,:,:,end], 28*28, :) * samples_h' / size(samples_v, 3))
-)
+Makie.scatter(vec(vh_data), vec(vh_model))
 Makie.xlims!(0,1)
 Makie.ylims!(0,1)
 Makie.current_figure()
 
+# Correlation
+
+cor(vec(vh_data), vec(vh_model))
+
 # Verify convergence of sampling
 
-Makie.lines(vec(mean(free_energy(rbm, samples_v); dims=1)))
-
-# Plot average data digit
-
-Makie.heatmap(mean(samples_v[:,:,:,end]; dims=3)[:,:,1])
+Makie.lines(vec(mean(free_energy(rbm, v_model[:,:,1:100,:]); dims=1)))
 
 # Plot average sampled digit
+
+Makie.heatmap(mean(v_model[:,:,:,end]; dims=3)[:,:,1])
+
+# Plot average data digit
 
 Makie.heatmap(mean(train_x; dims=3)[:,:,1])
 
 # Difference
 
-Makie.heatmap(mean(samples_v[:,:,:,end]; dims=3)[:,:,1] - mean(train_x; dims=3)[:,:,1])
+Makie.heatmap(mean(v_model[:,:,:,end]; dims=3)[:,:,1] - mean(train_x; dims=3)[:,:,1])
+
+# Plot weights  of maximum intensity.
+
+𝒫 = sortperm([norm(rbm.w[:,:,m]) for m in 1:nhidden]; rev=true);
+
+fig = Makie.Figure()
+for i = 1:7
+    ax = Makie.Axis(fig[1,i], width=50, height=50)
+    Makie.heatmap!(ax, rbm.w[:, :, 𝒫[i]])
+    Makie.hidedecorations!(ax)
+    Makie.hidespines!(ax)
+end
+Makie.resize_to_layout!(fig)
+fig
