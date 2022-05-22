e957adae0ed6e01f4d608cc398c15ee480ea2bbc
Documenter.jl <documenter@juliadocs.github.io>
2022-05-22 02:01:10 +0000

    build based on 34814b4

--- dev/literate/moment_match.jl
+++ dev/literate/moment_match.jl
@@ -7,14 +7,30 @@ However, due to biased nature of the PCD approximation, these conditions might n
 exactly in practice.
 =#
 
-import MKL # faster on Intel CPUs
+#import MKL # faster on Intel CPUs
 import Makie, CairoMakie
 import MLDatasets
 import Flux
 using Statistics: mean, cor
 using LinearAlgebra: norm
-using RestrictedBoltzmannMachines: RBM, Binary, sample_from_inputs, free_energy
-using RestrictedBoltzmannMachines: sample_v_from_v, sample_h_from_v, initialize!, pcd!
+using RestrictedBoltzmannMachines: RBM, Binary, sample_from_inputs, free_energy, log_pseudolikelihood
+using RestrictedBoltzmannMachines: sample_v_from_v, sample_h_from_v, initialize!, pcd!, minibatch_count
+
+function moving_average(A::AbstractArray, m::Int)
+    out = similar(A)
+    R = CartesianIndices(A)
+    Ifirst, Ilast = first(R), last(R)
+    I1 = m÷2 * oneunit(Ifirst)
+    for I in R
+        n, s = 0, zero(eltype(out))
+        for J in max(Ifirst, I-I1):min(Ilast, I+I1)
+            s += A[J]
+            n += 1
+        end
+        out[I] = s/n
+    end
+    return out
+end
 
 # load MNIST dataset
 
@@ -40,11 +56,48 @@ end
 
 nhidden = 64
 batchsize = 64
-nupdates = 10000
+nupdates = 20000
 epochs = train_nepochs(; nsamples = size(train_x, 3), nupdates, batchsize)
-rbm = RBM(Binary(Float,28,28), Binary(Float,nhidden), zeros(Float,28,28,nhidden))
-initialize!(rbm, train_x);
-@time pcd!(rbm, train_x; epochs, batchsize);
+rbm = RBM(Binary(Float,28,28), Binary(Float,nhidden), randn(Float,28,28,nhidden)/28)
+#initialize!(rbm, train_x);
+lpls = Float64[]
+Fm = zeros(batchsize, epochs * minibatch_count(train_x; batchsize))
+Fd = zeros(batchsize, epochs * minibatch_count(train_x; batchsize))
+t = lastepoch = 0
+function mycb(; epoch, vm, vd, kw...)
+    global lastepoch
+    global t += 1
+    Fm[:, t] .= free_energy(rbm, vm)
+    Fd[:, t] .= free_energy(rbm, vd)
+    if epoch > lastepoch
+        push!(lpls, mean(log_pseudolikelihood(rbm, train_x)))
+        lastepoch = epoch
+    end
+end
+@time pcd!(rbm, train_x; epochs, batchsize, callback=mycb, optim=Flux.ADAM(5e-4, (0.9, 0.999)));
+
+# Plot log_pseudolikelihood during training
+
+Makie.lines(lpls)
+
+# Average energies of data and model fantasy particles used during training.
+
+fig = Makie.Figure()
+ax = Makie.Axis(fig[1,1])
+Makie.lines!(ax, vec(mean(Fm; dims=1)), color=(:red, 0.25))
+Makie.lines!(ax, vec(mean(Fd; dims=1)), color=(:blue, 0.25))
+Makie.lines!(ax, moving_average(vec(mean(Fm; dims=1)), 30), color=:red, label="Fm")
+Makie.lines!(ax, moving_average(vec(mean(Fd; dims=1)), 30), color=:blue, label="Fd")
+Makie.axislegend(ax)
+fig
+
+# Difference in average energies of data and model fantasy particles used during training.
+
+fig = Makie.Figure()
+ax = Makie.Axis(fig[1,1])
+Makie.lines!(ax, vec(mean(Fm .- Fd; dims=1)), color=(:blue, 0.25))
+Makie.lines!(ax, moving_average(vec(mean(Fm .- Fd; dims=1)), 30), color=:red)
+fig
 
 # Generate samples, in v-space and h-space.
 
@@ -65,6 +118,15 @@ vh_data = reshape(v_data_center, 28*28, :) * h_data_center' / size(v_data_center
 v_model_center = v_model[:,:,:,end] .- mean(v_model[:,:,:,end]; dims=3);
 h_model_center = h_model .- mean(h_model; dims=2);
 vh_model = reshape(v_model_center, 28*28, :) * h_model_center' / size(v_model_center, 3);
+
+# Average energy of generated and data points
+
+fig = Makie.Figure()
+ax = Makie.Axis(fig[1,1])
+Makie.hist!(ax, free_energy(rbm, train_x), color=:purple, label="data", normalization=:pdf)
+Makie.hist!(ax, free_energy(rbm, v_model[:,:,:,end]), color=:orange, label="model", normalization=:pdf)
+Makie.axislegend(ax)
+fig
 
 # Plot moment-matching conditions
 
