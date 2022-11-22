04f03d73ccf1227d6c777fdd10f092c3ede0a3aa
Jorge Fernandez-de-Cossio-Diaz <cossio@users.noreply.github.com>
2022-11-22 17:28:38 +0100

    update docs

--- docs/src/literate/MNIST.jl
+++ docs/src/literate/MNIST.jl
@@ -8,12 +8,11 @@ We load MNIST via the MLDatasets.jl package.
 import Makie
 import CairoMakie
 import MLDatasets
-import RestrictedBoltzmannMachines as RBMs
 using Statistics: mean, std, var
 using Random: bitrand
-using ValueHistories: MVHistory
-using RestrictedBoltzmannMachines: BinaryRBM, sample_from_inputs, minibatch_count
-using RestrictedBoltzmannMachines: initialize!, log_pseudolikelihood, pcd!
+using ValueHistories: MVHistory, @trace
+using RestrictedBoltzmannMachines: BinaryRBM, sample_from_inputs, minibatch_count,
+    initialize!, log_pseudolikelihood, pcd!, free_energy, sample_v_from_v
 nothing #hide
 
 # Useful function to plot grids of MNIST digits.
@@ -67,23 +66,19 @@ Initially, the RBM assigns a poor pseudolikelihood to the data.
 println("log(PL) = ", mean(@time log_pseudolikelihood(rbm, train_x)))
 
 #=
-(Incidentally, note how long it takes to evaluate the pseudolikelihood on the full
-dataset.)
-
 Now we train the RBM on the data.
-This returns a [MVHistory](https://github.com/JuliaML/ValueHistories.jl)
-collecting some info during training.
 =#
 
 batchsize = 256
 batchcount = minibatch_count(train_x; batchsize)
-epochs = 500
+iters = 10000
 history = MVHistory()
 @time pcd!(
-    rbm, train_x; epochs, batchsize,
-    callback = function(; rbm, epoch, batch_idx, _...)
-        if batch_idx == batchcount && epoch % 5 == 0
-            push!(history, :lpl, mean(log_pseudolikelihood(rbm, train_x)))
+    rbm, train_x; iters, batchsize,
+    callback = function(; iter, _...)
+        if iszero(iter % 100)
+            lpl = mean(log_pseudolikelihood(rbm, train_x))
+            @trace history iter lpl
         end
     end
 )
@@ -102,10 +97,10 @@ fig
 nsteps = 3000
 fantasy_F = zeros(nrows*ncols, nsteps)
 fantasy_x = bitrand(28,28,nrows*ncols)
-fantasy_F[:,1] .= RBMs.free_energy(rbm, fantasy_x)
+fantasy_F[:,1] .= free_energy(rbm, fantasy_x)
 @time for t in 2:nsteps
-    fantasy_x .= RBMs.sample_v_from_v(rbm, fantasy_x)
-    fantasy_F[:,t] .= RBMs.free_energy(rbm, fantasy_x)
+    fantasy_x .= sample_v_from_v(rbm, fantasy_x)
+    fantasy_F[:,t] .= free_energy(rbm, fantasy_x)
 end
 nothing #hide
 
