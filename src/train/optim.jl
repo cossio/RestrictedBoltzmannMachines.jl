d0ad2cd618d95379ca0f9df99195a70fbba44e5c
Jorge Fernandez-de-Cossio-Diaz <cossio@users.noreply.github.com>
2022-12-07 17:50:18 +0100

    ∂RBM scalar division

--- src/train/optim.jl
+++ src/train/optim.jl
@@ -12,5 +12,6 @@ Base.:(+)(∂1::∂RBM, ∂2::∂RBM) = ∂RBM(∂1.visible + ∂2.visible, ∂1
 Base.:(-)(∂1::∂RBM, ∂2::∂RBM) = ∂RBM(∂1.visible - ∂2.visible, ∂1.hidden - ∂2.hidden, ∂1.w - ∂2.w)
 Base.:(*)(λ::Real, ∂::∂RBM) = ∂RBM(λ * ∂.visible, λ * ∂.hidden, λ * ∂.w)
 Base.:(*)(∂::∂RBM, λ::Real) = λ * ∂
+Base.:(/)(∂::∂RBM, λ::Real) = ∂RBM(∂.visible / λ, ∂.hidden / λ, ∂.w / λ)
 Base.:(==)(∂1::∂RBM, ∂2::∂RBM) = (∂1.visible == ∂2.visible) && (∂1.hidden == ∂2.hidden) && (∂1.w == ∂2.w)
 Base.hash(∂::∂RBM, h::UInt) = hash(∂.visible, hash(∂.hidden, hash(∂.w, h)))
--- test/optim.jl
+++ test/optim.jl
@@ -6,5 +6,6 @@ using RestrictedBoltzmannMachines: ∂RBM
     ∂2 = ∂RBM(randn(1,3), randn(1,2), randn(3,2))
     @test @inferred(∂1 + ∂2) == ∂RBM(∂1.visible + ∂2.visible, ∂1.hidden + ∂2.hidden, ∂1.w + ∂2.w)
     @test @inferred(∂1 - ∂2) == ∂RBM(∂1.visible - ∂2.visible, ∂1.hidden - ∂2.hidden, ∂1.w - ∂2.w)
-    @test @inferred(2∂1) == ∂RBM(2∂1.visible, 2∂1.hidden, 2∂1.w)
+    @test @inferred(2 * ∂1) == ∂RBM(2 * ∂1.visible, 2 * ∂1.hidden, 2 * ∂1.w)
+    @test @inferred(∂1 / 2) == ∂RBM(∂1.visible / 2, ∂1.hidden / 2, ∂1.w / 2)
 end
