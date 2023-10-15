4e485f439c314fead64ba5c37cf5f09f07f695ed
Jorge FdCD <cossio@users.noreply.github.com>
2023-10-15 16:27:27 +0200

    spaces

--- test/aqua.jl
+++ test/aqua.jl
@@ -6,8 +6,8 @@ using Test: @testset
 @testset "aqua" begin
     Aqua.test_all(
         RestrictedBoltzmannMachines;
-        stale_deps=(ignore=[:DiffRules],),
-        ambiguities=(exclude=[reshape],),
-        project_toml_formatting=false
+        stale_deps = (ignore = [:DiffRules],),
+        ambiguities = (exclude = [reshape],),
+        project_toml_formatting = false
     )
 end
