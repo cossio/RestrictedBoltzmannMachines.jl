3909eddd9ea64cab23b91e1aee1c01764e056c67
cossio <cossio@users.noreply.github.com>
2025-01-25 17:26:45 +0100

    std hdf5

--- ext/HDF5Ext.jl
+++ ext/HDF5Ext.jl
@@ -9,6 +9,7 @@ using RestrictedBoltzmannMachines: Potts
 using RestrictedBoltzmannMachines: PottsGumbel
 using RestrictedBoltzmannMachines: RBM
 using RestrictedBoltzmannMachines: Spin
+using RestrictedBoltzmannMachines: StandardizedRBM
 using RestrictedBoltzmannMachines: xReLU
 
 # Version of the file format used to save/load RBMs
@@ -54,6 +55,26 @@ function RestrictedBoltzmannMachines.save_rbm(path::AbstractString, rbm::RBM; ov
     return path
 end
 
+function RestrictedBoltzmannMachines.save_rbm(path::AbstractString, rbm::StandardizedRBM; overwrite::Bool=false)
+    if !overwrite && isfile(path)
+        error("File already exists: $path")
+    end
+    h5open(path, "w") do file
+        write(file, FILE_FORMAT_HEADER, string(FILE_FORMAT_VERSION))
+        write(file, "rbm_type", "StandardizedRBM")
+        write(file, "weights", rbm.w)
+        write(file, "visible_par", rbm.visible.par)
+        write(file, "hidden_par", rbm.hidden.par)
+        write(file, "visible_type", layer_type(rbm.visible))
+        write(file, "hidden_type", layer_type(rbm.hidden))
+        write(file, "offset_v", rbm.offset_v)
+        write(file, "offset_h", rbm.offset_h)
+        write(file, "scale_v", rbm.scale_v)
+        write(file, "scale_h", rbm.scale_h)
+    end
+    return path
+end
+
 layer_type(::Binary) = "Binary"
 layer_type(::Spin) = "Spin"
 layer_type(::Potts) = "Potts"
@@ -75,6 +96,17 @@ function _load_rbm(file::HDF5.File, ::Val{:RBM})
     visible = construct_layer(read(file, "visible_type"), read(file, "visible_par"))
     hidden = construct_layer(read(file, "hidden_type"), read(file, "hidden_par"))
     return RBM(visible, hidden, w)
+end
+
+function _load_rbm(file::HDF5.File, ::Val{:StandardizedRBM})
+    offset_v = read(file, "offset_v")
+    offset_h = read(file, "offset_h")
+    scale_v = read(file, "scale_v")
+    scale_h = read(file, "scale_h")
+    w = read(file, "weights")
+    visible = construct_layer(read(file, "visible_type"), read(file, "visible_par"))
+    hidden = construct_layer(read(file, "hidden_type"), read(file, "hidden_par"))
+    return StandardizedRBM(visible, hidden, w, offset_v, offset_h, scale_v, scale_h)
 end
 
 end
--- test/hdf5.jl
+++ test/hdf5.jl
@@ -7,6 +7,8 @@ using RestrictedBoltzmannMachines: PottsGumbel
 using RestrictedBoltzmannMachines: RBM
 using RestrictedBoltzmannMachines: save_rbm
 using RestrictedBoltzmannMachines: Spin
+using RestrictedBoltzmannMachines: standardize
+using RestrictedBoltzmannMachines: StandardizedRBM
 using RestrictedBoltzmannMachines: xReLU
 using Test: @test
 using Test: @testset
@@ -58,4 +60,32 @@ end
     @test loaded_rbm.w == rbm.w
     @test loaded_rbm.visible.θ == rbm.visible.θ
     @test loaded_rbm.hidden.θ == rbm.hidden.θ
+end
+
+@testset "std" begin
+    rbm = standardize(RBM(Binary(; θ=randn(2,3)), xReLU(; θ=randn(4), γ=randn(4), Δ=randn(4), ξ=randn(4)), randn(2,3,4)))
+    rbm.offset_v .= randn.()
+    rbm.offset_h .= randn.()
+    rbm.scale_v .= randn.()
+    rbm.scale_h .= randn.()
+
+    path = save_rbm(tempname(), rbm)
+    loaded_rbm = load_rbm(path)
+
+    @test loaded_rbm.visible isa Binary
+    @test loaded_rbm.hidden isa xReLU
+
+    @test loaded_rbm.w == rbm.w
+
+    @test loaded_rbm.visible.θ == rbm.visible.θ
+
+    @test loaded_rbm.hidden.θ == rbm.hidden.θ
+    @test loaded_rbm.hidden.γ == rbm.hidden.γ
+    @test loaded_rbm.hidden.Δ == rbm.hidden.Δ
+    @test loaded_rbm.hidden.ξ == rbm.hidden.ξ
+
+    @test loaded_rbm.offset_v == rbm.offset_v
+    @test loaded_rbm.offset_h == rbm.offset_h
+    @test loaded_rbm.scale_v == rbm.scale_v
+    @test loaded_rbm.scale_h == rbm.scale_h
 end
