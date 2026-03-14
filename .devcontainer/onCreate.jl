59026433e12588954c2601bd84cb8b95a94de36e
jorgefdcd <210008509+jorgefdcd@users.noreply.github.com>
2026-03-14 18:45:47 +0000

    rm devcontainer

--- .devcontainer/onCreate.jl
+++
@@ -1,9 +0,0 @@
-import Pkg
-
-Pkg.Registry.add(Pkg.RegistrySpec(url="https://github.com/cossio/CossioJuliaRegistry.git"))
-Pkg.Registry.add("General")
-
-Pkg.add([
-    "MyRegistrator",
-    "Revise",
-])
--- .devcontainer/postCreate.sh
+++
@@ -1,3 +0,0 @@
-# Authenticate GitHub CLI
-gh auth login -h github.com --with-token < /home/vscode/secrets/gh_token_classic.txt
-#gh auth setup-git
