2c1c7509385bd05fae093888b1655a01054cfb54
cossio <cossio@users.noreply.github.com>
2026-07-19 12:30:23 +0200

    Format ExplicitImports changes with Runic

--- src/util/truncated_normal.jl
+++ src/util/truncated_normal.jl
@@ -19,7 +19,7 @@ randnt(rng::AbstractRNG, a::BigFloat) = randnt(rng, Float64(a))
 randnt(a::Real) = randnt(default_rng(), a)
 
 # This union mirrors private `Base.IEEEFloat` without relying on a non-public name.
-function randnt(rng::AbstractRNG, a::Union{Float16,Float32,Float64})
+function randnt(rng::AbstractRNG, a::Union{Float16, Float32, Float64})
     local r = a
     if a ≤ 0
         while true
